bee78d186b0996cc25fb2ba10ba0025603ade457
Kang-Soo Lee <kipacti@gmail.com>
2024-02-08 05:23:45 +0900

    Time: 59 ms (29.72%) | Memory: 49.2 MB (12.55%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,17 +6,27 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    for (let i = 0, j = 0; i < m + n; i++) {
-        if (j < n) {
-            if (i === m + j) {
-                nums1.splice(i, 1, nums2[j]);
-                j++;
-            }
-            else if (nums1[i] > nums2[j]) {
-                nums1.splice(i, 0, nums2[j]);
-                nums1.splice(m + n, 1);
-                j++;
-            }
+    last = m + n - 1;
+
+    while(m > 0 && n > 0){
+        if(nums1[m - 1] > nums2[n - 1]){
+            nums1[last] = nums1[m - 1];
+            m--;
         }
+        else{
+            nums1[last] = nums2[n - 1];
+            n--;
+        }
+
+        last--;
+    }
+
+
+    // left over in second array
+
+    while(n > 0){
+        nums1[last] = nums2[n - 1];
+        last--;
+        n--;
     }
 };
