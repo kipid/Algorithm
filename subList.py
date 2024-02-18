a38812ba481813fca1e903ccdb2c4502ad34b1e8
Kang-Soo Lee <kipacti@gmail.com>
2024-02-19 08:50:10 +0900

    subList.py study.

---
+++ subList.py
@@ -0,0 +1,15 @@
+list = [0,1,2,3,4,5,6,7,8,9]
+print(list[1:]) # [1,2,3,4,5,6,7,8,9]
+print(list[:3]) # [0,1,2]
+print(list[3:5]) # [3,4]
+print(list[3:5:-1]) # []
+print(list[5:3:-1]) # [5,4]
+print(list[3::-1]) # [3,2,1,0]
+print(list[3:-1:-1]) # []
+print(range(3,-1,-1)) # range(3,-1,-1)
+for i in range(3,-1,-1):
+	print(f"i: {i}") # 3,2,1,0
+print()
+print(list[3:0:-1]) # [3,2,1]
+print(list[:3:-1]) # [9,8,7,6,5,4]
+print(list[::-1]) # [9,8,7,6,5,4,3,2,1,0]
