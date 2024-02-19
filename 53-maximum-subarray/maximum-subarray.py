21d98508e4412cc642f96f1b68f7dc82f8764de5
Kang-Soo Lee <kipacti@gmail.com>
2024-02-19 10:05:50 +0900

    Time: 93 ms (99.97%) | Memory: 26.4 MB (99.97%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,22 +1,22 @@
 def solve():
-    def work(start_idx, end_idx):
-        if start_idx == end_idx:
-            if (val := nums[start_idx]) > work.greatest_sum:
-                work.greatest_sum = val
-            return val, val, val
+    # def work(start_idx, end_idx):
+    #     if start_idx == end_idx:
+    #         if (val := nums[start_idx]) > work.greatest_sum:
+    #             work.greatest_sum = val
+    #         return val, val, val
 
-        pivot_idx = start_idx + (end_idx - start_idx) // 2
-        val_left_sum_left,  val_left_sum_whole,  val_left_sum_right = work(start_idx, pivot_idx)
-        val_right_sum_left,  val_right_sum_whole,  val_right_sum_right = work(pivot_idx + 1, end_idx)
+    #     pivot_idx = start_idx + (end_idx - start_idx) // 2
+    #     val_left_sum_left,  val_left_sum_whole,  val_left_sum_right = work(start_idx, pivot_idx)
+    #     val_right_sum_left,  val_right_sum_whole,  val_right_sum_right = work(pivot_idx + 1, end_idx)
 
-        whole = val_left_sum_whole + val_right_sum_whole
-        left = max(val_left_sum_left, val_left_sum_whole, val_left_sum_whole + val_right_sum_left, whole)
-        right = max(whole, val_left_sum_right + val_right_sum_whole, val_right_sum_whole, val_right_sum_right)
+    #     whole = val_left_sum_whole + val_right_sum_whole
+    #     left = max(val_left_sum_left, val_left_sum_whole, val_left_sum_whole + val_right_sum_left, whole)
+    #     right = max(whole, val_left_sum_right + val_right_sum_whole, val_right_sum_whole, val_right_sum_right)
 
-        if (max_ := max(left, whole, right, val_left_sum_right + val_right_sum_left)) > work.greatest_sum:
-            work.greatest_sum = max_
+    #     if (max_ := max(left, whole, right, val_left_sum_right + val_right_sum_left)) > work.greatest_sum:
+    #         work.greatest_sum = max_
 
-        return left, whole, right
+    #     return left, whole, right
 
     f = open('user.out', 'w')
     for nums in map(loads, stdin):
