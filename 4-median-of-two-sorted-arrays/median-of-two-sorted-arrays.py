4120e02cfcd8e21e47c436cd9240b5780de5ed75
Kang-Soo Lee <kipacti@gmail.com>
2024-02-23 11:21:22 +0900

    Time: 88 ms (28.56%) | Memory: 16.9 MB (61.64%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -3,7 +3,6 @@ class Solution:
         merged = []
         i, j, sum = 0, 0, len(nums1) + len(nums2)
         mid = sum // 2
-        midFound = False
         while i < len(nums1) and j < len(nums2):
             if nums1[i] <= nums2[j]:
                 merged.append(nums1[i])
@@ -11,13 +10,12 @@ class Solution:
             else:
                 merged.append(nums2[j])
                 j += 1
-        if not midFound:
-            while i < len(nums1):
-                merged.append(nums1[i])
-                i += 1
-            while j < len(nums2):
-                merged.append(nums2[j])
-                j += 1
+        while i < len(nums1):
+            merged.append(nums1[i])
+            i += 1
+        while j < len(nums2):
+            merged.append(nums2[j])
+            j += 1
         if sum % 2 == 0:
             return (merged[mid] + merged[mid - 1]) / 2
         else:
