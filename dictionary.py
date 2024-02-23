7b7a1840e6f772e004735b40125a2cc8c59e4d7c
Kang-Soo Lee <kipacti@gmail.com>
2024-02-23 10:19:19 +0900

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -7,3 +7,12 @@ print(True if "a" in dict_ else False)
 s = "abcdefghijklmnopqrstuvwxyz"
 for r, let in enumerate(s):
 	print(f"r: {r}, let: {let}")
+
+print()
+seen = dict()
+print(seen)
+for r, let in enumerate(s):
+	seen[let] = r
+print(seen)
+print(seen.get("c", -1))
+print(seen.get("!", -2)) # default value -2
