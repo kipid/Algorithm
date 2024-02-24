99d2c2aa8b704bc5e0568b8ee2cd5c1a52bf13c2
Kang-Soo Lee <kipacti@gmail.com>
2024-02-24 11:59:47 +0900

    Time: 341 ms (13.05%) | Memory: 16.6 MB (87.33%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -0,0 +1,7 @@
+import re
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        p = re.sub(r"\*+", "*", p)
+        pattern = re.compile("^"+p+"$")
+        return pattern.search(s)
