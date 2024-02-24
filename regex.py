094b8126d13b9c84503db4621892e2f3fcc3160b
Kang-Soo Lee <kipacti@gmail.com>
2024-02-24 13:09:20 +0900

    study regex.

--- regex.py
+++ regex.py
@@ -1,4 +1,3 @@
-# https://www.youtube.com/watch?v=wnuBwl2ekmo
 # Regular Expressions in Python
 
 import re
@@ -12,24 +11,35 @@ print(pattern.search("HELLOWORLD"))
 match = pattern.search("HELLOWORLD")
 print(match) # <re.Match object; span=(0, 10), match='HELLOWORLD'>
 print(match.group(0)) # HELLOWORLD
-# print(match.group(1))
+# print(match.group(1)) # IndexError: no such group
 print()
 
+pattern = re.compile("[A-Z]{3}")
 matches = pattern.finditer("HELLOWORLD")
 print(matches) # <callable_iterator object at 0x000001CBF4327E20>
 print()
 
 for m in matches:
-	print(m) # <re.Match object; span=(0, 10), match='HELLOWORLD'>
+	print(m)
+"""
+<re.Match object; span=(0, 3), match='HEL'>
+<re.Match object; span=(3, 6), match='LOW'>
+<re.Match object; span=(6, 9), match='ORL'>
+"""
 print()
 
 findall = pattern.findall("HELLOWORLD")
-print(findall) # ['HELLOWORLD']
+print(findall) # ['HEL', 'LOW', 'ORL']
 
 pattern1 = re.compile("[^\\w]+")
 findall = pattern1.findall("This: is a pencil.")
 print(findall) # [': ', ' ', ' ', '.']
-str = "This: is a pencil."
+str = "This: is a pencil_."
 strReplaced = re.sub(r"[^\w]+", "", str).lower()
-print(strReplaced) # thisisapencil
-print(strReplaced[::-1]) # licnepasisiht
+print(strReplaced) # thisisapencil_
+print(strReplaced[::-1]) # _licnepasisiht
+
+print()
+p = "a***"
+p = re.sub(r"\*+", "*", p)
+print(p)
--- test-problem.py
+++ test-problem.py
@@ -1,17 +1,5 @@
-class Solution:
-    def reverse(self, x: int) -> int:
-        isMinus = (x < 0)
-        if isMinus:
-            x = -x
-        res = x % 10
-        print(f"res: {res}")
-        while (x := x // 10) > 0:
-            print(f"res: {res}, x: {x}")
-            res = 10 * res + x % 10
-        if isMinus:
-            res = -res
-        return res
+import re
 
-sol = Solution()
-print(sol.reverse(123))
-
+p = "a***"
+p = re.sub(r"\*+", "*", p)
+print(p)
