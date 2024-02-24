418fd57c9725e4813e4830813372c0b3504c7c64
Kang-Soo Lee <kipacti@gmail.com>
2024-02-24 16:16:36 +0900

    Time: 39 ms (90.74%) | Memory: 16.6 MB (82.81%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,20 +1,16 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         dict_ = {
-            "I":1, "VI":4, "V":5, "XI":9,
-            "X":10, "LX":40, "L":50, "CX":90,
-            "C":100, "DC":400, "D":500, "MC":900, "M":1000
+            "I":1, "V":5,
+            "X":10, "L":50,
+            "C":100, "D":500,
+            "M":1000
         }
-        i = 0
         sum = 0
-        revS = s[::-1]
-        while i < len(revS) - 1:
-            if revS[i:i+2] in dict_:
-                sum += dict_[revS[i:i+2]]
-                i += 2
-            elif revS[i:i+1] in dict_:
-                sum += dict_[revS[i:i+1]]
-                i += 1
-        if i < len(revS):
-            sum += dict_[revS[i:i+1]]
+        for i in range(len(s)-1):
+            if dict_[s[i+1:i+2]] > dict_[s[i:i+1]]:
+                sum -= dict_[s[i:i+1]]
+            else:
+                sum += dict_[s[i:i+1]]
+        sum += dict_[s[len(s)-1:]]
         return sum
