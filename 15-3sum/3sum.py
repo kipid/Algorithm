4e34b2ee3d50affef36691ab22ddf4d042005bbf
Kang-Soo Lee <kipacti@gmail.com>
2024-02-25 10:49:52 +0900

    Time: 642 ms (74.82%) | Memory: 20.7 MB (68.00%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,39 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
-        nums = sorted(nums)
-        dict_nums = {}
-        dict_last_num = {}
-        for i in range(len(nums)):
-            dict_last_num[nums[i]] = i
-            if nums[i] == 0:
-                if 0 in dict_nums:
-                    dict_nums[0].append(i)
-                else:
-                    dict_nums[0]=[i]
-        if 0 in dict_nums and len(dict_nums[0]) >= 3:
-            res.append([0,0,0])
-        dict_first_num = {}
-        for i in range(len(nums)-1, -1, -1):
-            dict_first_num[nums[i]] = i
-        i = 0
-        while i < len(nums)-2:
-            if dict_first_num[nums[i]] != i:
-                i += 1
+        nums.sort()
+
+        for i , a in enumerate(nums):
+            if i > 0 and nums[i-1] == a:
                 continue
-            j = i+1
-            while j < len(nums)-1:
-                numK = -nums[i]-nums[j]
-                if numK in dict_last_num:
-                    if nums[i] == nums[j] and dict_first_num[nums[j]] + 1 == j:
-                        if nums[j] < numK:
-                            res.append([nums[i], nums[j], numK])
-                    elif dict_first_num[nums[j]] == j:
-                        if nums[j] < numK:
-                            res.append([nums[i], nums[j], numK])
-                        elif nums[j] == numK:
-                            if dict_last_num[nums[j]] > j:
-                                res.append([nums[i], nums[j], numK])
-                j += 1
-            i += 1
+            
+            p1 = i + 1
+            p2 = len(nums) - 1
+
+            while p1 < p2:
+                k = a + nums[p1] + nums[p2]
+                if k > 0:
+                    p2 -= 1
+                elif k < 0:
+                    p1 += 1
+                else:
+                    res.append([a,nums[p1],nums[p2]])
+                    p1 += 1
+                    while nums[p1] == nums[p1-1] and p1 < p2:
+                        p1 +=1
+
         return res
