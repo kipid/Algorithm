5a143297ee3585877e284f9a20456bda24d50f22
Kang-Soo Lee <kipacti@gmail.com>
2024-03-01 21:31:35 +0900

    Time: 52 ms (9.95%) | Memory: 16.6 MB (67.14%) - LeetSync

---
+++ 31-next-permutation/next-permutation.py
@@ -0,0 +1,33 @@
+class Solution:
+    def swap(self, nums: List[int], i: int, j: int) -> None:
+        temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
+    def reverse(self, nums: List[int], start: int) -> None:
+        i = start
+        j = len(nums) - 1
+        while i < j:
+            self.swap(nums, i, j)
+            i += 1
+            j -= 1
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 1
+        while i > 0:
+            if nums[i-1] < nums[i]:
+                break
+            i -= 1
+        p = i - 1
+        if p == -1:
+            nums.sort()
+            return
+        i = len(nums) - 1
+        while i > 0:
+            if nums[p] < nums[i]:
+                break
+            i -= 1
+        self.swap(nums, p, i)
+        self.reverse(nums, p+1)
+        return
