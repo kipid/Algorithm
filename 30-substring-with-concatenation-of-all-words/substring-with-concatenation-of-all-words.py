7090aa62939b095d20d899437ac5d7ed1391e21e
Kang-Soo Lee <kipacti@gmail.com>
2024-03-01 13:14:36 +0900

    Time: 3993 ms (42.11%) | Memory: 17.4 MB (40.62%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -15,17 +15,16 @@ class Solution:
             k = s.find(key, i)
             while k != -1 and k <= maxIndex:
                 dict_copy = dict()
-                for key0,val0 in dict_.items():
-                    dict_copy[key0] = val0
-                dict_copy[key] -= 1
+                # for key0,val0 in dict_.items():
+                #     dict_copy[key0] = val0
+                dict_copy[key] = 1
                 i = k
                 matched = True
                 for _ in range(1, len(words)):
                     k += wordsLength
                     key1 = s[k:k+wordsLength]
-                    if key1 in dict_copy and dict_copy[key1] > 0:
-                        dict_copy[key1] -= 1
-                    else:
+                    dict_copy[key1] = dict_copy.get(key1, 0) + 1
+                    if dict_copy[key1] > dict_.get(key1, 0):
                         matched = False
                         break
                 if matched:
