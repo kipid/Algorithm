938fe5ce9c2ec695d46efba30ed39c1481eb343d
Kang-Soo Lee <kipacti@gmail.com>
2024-03-02 13:31:39 +0900

    Time: 46 ms (43.04%) | Memory: 17 MB (69.33%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -3,7 +3,7 @@ class Solution:
         left, right = 0, len(nums) - 1
     
         while left <= right:
-            mid = left + (right - left) // 2
+            mid = (right + left) // 2
             
             if nums[mid] == target:
                 return mid
