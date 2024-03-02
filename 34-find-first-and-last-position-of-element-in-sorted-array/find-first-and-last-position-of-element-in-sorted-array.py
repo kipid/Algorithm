0b07152f2a9004f9c6cec61a96927b9af60a0e09
Kang-Soo Lee <kipacti@gmail.com>
2024-03-02 15:54:21 +0900

    Time: 62 ms (99.04%) | Memory: 18 MB (37.15%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,47 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left, right = 0, len(nums)-1
+        if len(nums) > 0:
+            if nums[left] == target:
+                right = left
+                while right < len(nums) and nums[right] == target:
+                    right += 1
+                return [left, right - 1]
+            elif nums[right] == target:
+                left = right
+                while left >= 0 and nums[left] == target:
+                    left -= 1
+                return [left + 1, right]
+            elif target < nums[left] or nums[right] < target:
+                return [-1, -1]
+        while left <= right:
+            mid = (right + left) // 2
+
+            if nums[mid] == target:
+                left = mid
+                while left >= 0 and nums[left] == target:
+                    left -= 1
+                right = mid
+                while right < len(nums) and nums[right] == target:
+                    right += 1
+                return [left+1, right-1]
+            elif nums[left] == target:
+                right = left
+                while right < len(nums) and nums[right] == target:
+                    right += 1
+                return [left, right - 1]
+            elif nums[right] == target:
+                left = right
+                while left >= 0 and nums[left] == target:
+                    left -= 1
+                return [left + 1, right]
+
+            if nums[left] < target < nums[mid]:
+                right = mid - 1
+            elif nums[mid] < target < nums[right]:
+                left = mid + 1
+            else:
+                return [-1, -1]
+
+        
+        return [-1, -1]
