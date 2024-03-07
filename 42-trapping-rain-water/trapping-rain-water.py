f489e931ddb189d8364c09fe208b9037d8a9ff10
Kang-Soo Lee <kipacti@gmail.com>
2024-03-07 00:46:49 +0000

    Time: 86 ms (97.94%) | Memory: 18.4 MB (94.62%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,33 +1,23 @@
 class Solution:
     def trap(self, height: list[int]) -> int:
-        left, right = 0, len(height)-1
-        while left < right and height[left] <= height[left+1]:
-            left += 1
-        while right > left and height[right] <= height[right-1]:
-            right -= 1
+        l = 0
+        r = len(height) - 1
+        leftwall = 0
+        rightwall = 0
 
-        def fillWater(left: int, right: int) -> int: # waterFilled
-            if left >= right-1:
-                return 0
-            l = left
-            while l < right and height[l] >= height[l+1]:
+        trapped_rain = 0
+        while l <= r:
+            if height[l] <= height[r]:
+                if height[l] >= leftwall:
+                    leftwall = height[l]
+                else:
+                    trapped_rain += (leftwall-height[l])
                 l += 1
-            r = l
-            while r < right and height[r] <= height[r+1]:
-                r += 1
-            minHeight = min(height[left], height[r])
-            waterFilled = 0
-            for i in range(left+1, r):
-                if height[i] < minHeight:
-                    waterFilled += minHeight - height[i]
-                    height[i] = minHeight
-            # print(height[left:right+1])
-            # print(f"{waterFilled = }, {r = }, {right = }")
-            return waterFilled + fillWater(r, right)
+            else:
+                if height[r] >= rightwall:
+                    rightwall = height[r]
+                else:
+                    trapped_rain += (rightwall-height[r])
+                r -= 1
 
-        water = fillWater(left, right)
-        fullWater = water
-        while water > 0:
-            water = fillWater(left, right)
-            fullWater += water
-        return fullWater
+        return trapped_rain
