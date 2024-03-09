09a75ce65449a8573fbf67f45fd8da24e64f30d8
Kang-Soo Lee <kipacti@gmail.com>
2024-03-09 02:25:41 +0000

    Time: 43 ms (92.09%) | Memory: 16.6 MB (94.16%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -1,18 +1,18 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        sIndex,pIndex,starIndex,m=0,0,-1,0
+        sIndex,pIndex,starIndex,sMark=0,0,-1,0
         while sIndex<len(s):
             if pIndex<len(p) and (s[sIndex]==p[pIndex] or p[pIndex]=='?'):
                 pIndex+=1
                 sIndex+=1
             elif pIndex<len(p) and p[pIndex]=='*':
                 starIndex=pIndex
-                m=sIndex
+                sMark=sIndex
                 pIndex+=1
             elif starIndex!=-1:
                 pIndex=starIndex+1
-                m+=1
-                sIndex=m
+                sMark+=1
+                sIndex=sMark
             else:
                 return False
         while pIndex<len(p) and p[pIndex]=='*':
