241444a22e9af09238dd8fc26acbc6b664071ccc
Kang-Soo Lee <kipacti@gmail.com>
2024-03-09 04:28:22 +0000

    Time: 32 ms (94.97%) | Memory: 16.8 MB (48.39%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        set_ = set(nums)
+        res = []
+        def addNum(setNum: set[int], path: List[int]):
+            newSetNum0 = set(setNum)
+            for num in newSetNum0:
+                path.append(num)
+                newSetNum1 = set(newSetNum0)
+                newSetNum1.remove(num)
+                print(f"{newSetNum1 = }, {path = }")
+                if len(newSetNum1) > 0:
+                    addNum(newSetNum1, path)
+                else:
+                    res.append(list(path))
+                path.pop()
+        addNum(set_, [])
+        return res
