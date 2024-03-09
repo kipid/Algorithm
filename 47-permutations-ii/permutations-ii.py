32f2e91619d67d7211112efa446c861d10b9e8bb
Kang-Soo Lee <kipacti@gmail.com>
2024-03-09 05:12:09 +0000

    Time: 55 ms (56.76%) | Memory: 17.1 MB (28.52%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        dict_ = dict()
+        for num in nums:
+            if num in dict_:
+                dict_[num] += 1
+            else:
+                dict_[num] = 1
+        res = []
+        
+        def addNum(dictNums: dict, path: list[int]):
+            dictNums0 = dict(dictNums)
+            for num, count in dictNums0.items():
+                newDictNums = dict(dictNums0)
+                if newDictNums[num] > 0:
+                    newDictNums[num] -= 1
+                    path.append(num)
+                    if newDictNums[num] == 0:
+                        del newDictNums[num]
+                    # print(f"{newDictNums = }, {path = }")
+                    if len(newDictNums) > 0:
+                        addNum(newDictNums, path)
+                    else:
+                        res.append(list(path))
+                    path.pop()
+            
+        addNum(dict_, [])
+        return res
