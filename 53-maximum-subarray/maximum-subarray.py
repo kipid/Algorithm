994db1ed6f0fffab860dd2106c376f4553e8533a
Kang-Soo Lee <kipacti@gmail.com>
2024-03-10 05:38:13 +0000

    Time: 2686 ms (5.02%) | Memory: 31.1 MB (30.42%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,14 +1,46 @@
-def solve():
-    f = open('user.out', 'w')
-    for nums in map(loads, stdin):
-        maxSum, curSum = nums[0], nums[0]
-        for num in nums[1:]:
-            if curSum < 0:
-                curSum = num
-            else:
-                curSum += num
-            maxSum = max(maxSum, curSum)
-        print(maxSum, file=f)
-
-solve()
-exit()
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        lenNums = len(nums)
+        if lenNums == 1:
+            return nums[0]
+        start = 0
+        endEnd = lenNums - 1
+        atLeastOnePositive = False
+        while start < lenNums:
+            if nums[start] > 0:
+                atLeastOnePositive = True
+                break
+            start += 1
+        if atLeastOnePositive:
+            while endEnd >= start:
+                if nums[endEnd] > 0:
+                    break
+                endEnd -= 1
+            maxSum = 0
+            while start <= endEnd:
+                sum = 0
+                if nums[start] <= 0:
+                    start += 1
+                    continue
+                end = start
+                while end <= endEnd:
+                    sum += nums[end]
+                    if sum <= 0:
+                        start = end
+                        break
+                    elif sum > maxSum:
+                        maxSum = sum
+                    end += 1
+                if start > 0:
+                    while start <= endEnd and nums[start] >= 0 and nums[start - 1] >= 0:
+                        start += 1
+                start += 1
+            return maxSum
+        else:
+            maxSum = nums[0]
+            start = 1
+            while start < lenNums:
+                if nums[start] > maxSum:
+                    maxSum = nums[start]
+                start += 1
+            return maxSum
