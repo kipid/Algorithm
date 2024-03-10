a60fc2ca84c793a18d4962305a13542e3538aeaf
Kang-Soo Lee <kipacti@gmail.com>
2024-03-10 05:40:57 +0000

    Time: 511 ms (80.49%) | Memory: 31 MB (69.64%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,46 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        lenNums = len(nums)
-        if lenNums == 1:
-            return nums[0]
-        start = 0
-        endEnd = lenNums - 1
-        atLeastOnePositive = False
-        while start < lenNums:
-            if nums[start] > 0:
-                atLeastOnePositive = True
-                break
-            start += 1
-        if atLeastOnePositive:
-            while endEnd >= start:
-                if nums[endEnd] > 0:
-                    break
-                endEnd -= 1
-            maxSum = 0
-            while start <= endEnd:
-                sum = 0
-                if nums[start] <= 0:
-                    start += 1
-                    continue
-                end = start
-                while end <= endEnd:
-                    sum += nums[end]
-                    if sum <= 0:
-                        start = end
-                        break
-                    elif sum > maxSum:
-                        maxSum = sum
-                    end += 1
-                if start > 0:
-                    while start <= endEnd and nums[start] >= 0 and nums[start - 1] >= 0:
-                        start += 1
-                start += 1
-            return maxSum
-        else:
-            maxSum = nums[0]
-            start = 1
-            while start < lenNums:
-                if nums[start] > maxSum:
-                    maxSum = nums[start]
-                start += 1
-            return maxSum
+        curr_sum = nums[0]
+        max_sum = nums[0]
+        for num in nums[1:]:
+            if curr_sum < 0:
+                curr_sum = num
+            else:
+                curr_sum += num
+            max_sum = max(max_sum, curr_sum) # 모든 List[int] 가 minus 일 때도 제대로 동작.
+        return max_sum
