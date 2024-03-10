f262e6d7f286338e6a6f16ee63b3f570fd087c90
Kang-Soo Lee <kipacti@gmail.com>
2024-03-10 01:47:15 +0000

    Time: 35 ms (57.75%) | Memory: 16.6 MB (76.65%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,3 +1,16 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return pow(x, n)
+        if n == 0:
+            return 1.0
+        
+        def powPositive(x: float, n: int) -> float:
+            if n == 1:
+                return x
+            elif n % 2 == 0:
+                return powPositive(x*x, n // 2)
+            else:
+                return x * powPositive(x*x, n // 2)
+
+        if n < 0:
+            return 1.0 / powPositive(x, -n)
+        return powPositive(x, n)
