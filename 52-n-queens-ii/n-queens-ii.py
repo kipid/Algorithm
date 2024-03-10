0d9ed9eaa4456e86f492d4e5f8963bcbd3501da7
Kang-Soo Lee <kipacti@gmail.com>
2024-03-10 05:21:38 +0000

    Time: 45 ms (67.91%) | Memory: 16.4 MB (86.46%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        res = 0
+        colSet = set()
+        posDiag = set()
+        negDiag = set()
+
+        def addQueen(row: int):
+            nonlocal res
+            if row >= n:
+                res += 1
+                return
+            for col in range(n):
+                if col in colSet or (col-row) in posDiag or (col+row) in negDiag:
+                    continue
+                colSet.add(col)
+                posDiag.add(col-row)
+                negDiag.add(col+row)
+                addQueen(row+1)
+                colSet.remove(col)
+                posDiag.remove(col-row)
+                negDiag.remove(col+row)
+        
+        addQueen(0)
+        return res
