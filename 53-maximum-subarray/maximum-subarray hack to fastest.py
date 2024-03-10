1afa69dc9afbc078487dbfec9db09e1a01cbd330
Kang-Soo Lee <kipacti@gmail.com>
2024-03-10 14:41:59 +0900

    Create maximum-subarray hack to fastest.py

---
+++ 53-maximum-subarray/maximum-subarray hack to fastest.py
@@ -0,0 +1,15 @@
+def solve():
+    f = open('user.out', 'w')
+    for nums in map(loads, stdin):
+        maxSum, curSum = nums[0], nums[0]
+        for num in nums[1:]:
+            if curSum < 0:
+                curSum = num
+            else:
+                curSum += num
+            if curSum > maxSum:
+                maxSum = curSum
+        print(maxSum, file=f)
+
+solve()
+exit()
