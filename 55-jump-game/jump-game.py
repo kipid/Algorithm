daa602d39e9c59212b580f1911e133782e10f491
Kang-Soo Lee <kipacti@gmail.com>
2024-03-10 07:00:36 +0000

    Time: 390 ms (25.90%) | Memory: 17.9 MB (64.99%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        def maxJump(left: int, right: int) -> int:
+            maxJumpTo = right
+            for i in range(left,right+1):
+                maxJumpTo = max(maxJumpTo, i+nums[i])
+            return maxJumpTo
+        left0, right0, n = 0, 0, len(nums)
+        # if n == 1:
+        #     return True
+        while (maxJumpTo := maxJump(left0, right0)) < n-1:
+            if maxJumpTo == right0:
+                return False
+            left0, right0 = right0, maxJumpTo
+        return True
