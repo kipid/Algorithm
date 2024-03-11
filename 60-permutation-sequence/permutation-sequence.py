e52ced228c86dbab78040cbfb21c43c5aba56507
Kang-Soo Lee <kipacti@gmail.com>
2024-03-11 09:12:08 +0000

    Time: 30 ms (92.48%) | Memory: 16.9 MB (31.06%) - LeetSync

---
+++ 60-permutation-sequence/permutation-sequence.py
@@ -0,0 +1,22 @@
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        k -= 1
+        listN = list()
+        for i in range(1,n+1):
+            listN.append(i)
+        def facto(i: int) -> int:
+            if i == 1:
+                return 1
+            return i * facto(i-1)
+        factoList = [1]
+        for i in range(1,n):
+            factoList.append(i * factoList[i-1])
+        res = []
+        while len(listN) > 0:
+            firstIndex = k // factoList[n-1]
+            # print(f"{k = }")
+            res.append(listN[firstIndex])
+            del listN[firstIndex]
+            k = k % factoList[n-1]
+            n -= 1
+        return "".join(map(str, res))
