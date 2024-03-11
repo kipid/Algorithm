a11f2df6925e50074a4b9b2d92ab46dfedaedf5b
Kang-Soo Lee <kipacti@gmail.com>
2024-03-11 04:42:38 +0000

    Time: 76 ms (53.60%) | Memory: 19.9 MB (45.85%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,54 +1,18 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        left1, right1 = 0, len(intervals)-1
-        left2, right2 = 0, len(intervals)-1
-        if len(intervals) == 0:
-            return [newInterval]
-
-        def findLeftInterval(left: int, right: int) -> int:
-            print(f"findLeft :: {left = }, {right = }")
-            if left >= right-1:
-                if newInterval[0] <= intervals[left][1]:
-                    return left
-                else:
-                    return right
-            mid = (left + right) // 2
-            if newInterval[0] <= intervals[mid][1]:
-                return findLeftInterval(left, mid)
+        result = []
+        flag = False
+        for interval in intervals:
+            if interval[1] < newInterval[0]:
+                result.append(interval)
+            elif interval[0] > newInterval[1]:
+                if not flag:
+                    result.append(newInterval)
+                    flag = True
+                result.append(interval)
             else:
-                return findLeftInterval(mid, right)
-        
-        def findRightInterval(left: int, right: int) -> int:
-            print(f"findRight :: {left = }, {right = }")
-            if left >= right-1:
-                if newInterval[1] < intervals[right][0]:
-                    return left
-                else:
-                    return right
-            mid = (left + right) // 2
-            if newInterval[1] < intervals[mid][0]:
-                return findRightInterval(left, mid)
-            else:
-                return findRightInterval(mid, right)
-
-        if newInterval[0] <= intervals[left1][1]:
-            right2 = findRightInterval(left2, right2)
-            print(f"{right2 = }")
-            if right2 == left2 and newInterval[1] < intervals[left2][0]:
-                return [newInterval] + intervals
-            else:
-                return [[min(intervals[left1][0], newInterval[0]), max(intervals[right2][1], newInterval[1])]] + intervals[right2+1:]
-        elif newInterval[0] > intervals[right1][1]:
-            return intervals + [newInterval]
-        if newInterval[1] < intervals[left2][0]:
-            return [newInterval] + intervals
-        elif newInterval[1] >= intervals[right2][0]:
-            left1 = findLeftInterval(left1, right1)
-            if left1 == right1 and newInterval[0] > intervals[right1][1]:
-                return intervals + [newInterval]
-            else:
-                return intervals[:left1] + [[min(intervals[left1][0], newInterval[0]), max(intervals[right2][1], newInterval[1])]]
-        
-        left1 = findLeftInterval(left1, right1)
-        right2 = findRightInterval(left2, right2)
-        return intervals[:left1] + [[min(intervals[left1][0], newInterval[0]), max(intervals[right2][1], newInterval[1])]] + intervals[right2+1:]
+                newInterval[0] = min(interval[0], newInterval[0])
+                newInterval[1] = max(newInterval[1], interval[1])
+        if not flag:
+            result.append(newInterval); 
+        return result
