e251b97d5df49e051a214a9295b17cb7ad301c52
Kang-Soo Lee <kipacti@gmail.com>
2024-03-12 13:26:15 +0000

    Time: 40 ms (43.78%) | Memory: 16.6 MB (33.09%) - LeetSync

--- 65-valid-number/valid-number.py
+++ 65-valid-number/valid-number.py
@@ -2,7 +2,7 @@ import re
 
 class Solution:
     def isNumber(self, s: str) -> bool:
-        pattern0 = re.compile("^[\+\-]?(\d+?\.?\d+?|\d*\.?\d+?|\d+?\.?\d*)([eE][\+\-]?\d+)?$")
+        pattern0 = re.compile("^[\+\-]?(\d*\.?\d+?|\d+?\.?\d*)([eE][\+\-]?\d+)?$")
         if pattern0.search(s):
             return True
         return False
