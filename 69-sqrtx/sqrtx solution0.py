9b3bd2f206810dc97e48a03492f7c7d57de14685
Kang-Soo Lee <kipacti@gmail.com>
2024-03-14 10:58:02 +0900

    Create sqrtx solution0.py

---
+++ 69-sqrtx/sqrtx solution0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        def findSqrt(left: int, right: int) -> int:
+            print(f"{left = }, {right = }")
+            if left >= right - 1:
+                return right - 1
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid > x:
+                return findSqrt(left, mid)
+            else:
+                return findSqrt(mid, right)
+        cand = findSqrt(0, x)
+        if (cand+1) * (cand+1) <= x:
+            return cand+1
+        else:
+            return cand
