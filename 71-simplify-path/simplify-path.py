6914b1b6202a5cbb638781e6256ba19a18f2cd25
Kang-Soo Lee <kipacti@gmail.com>
2024-03-14 04:48:57 +0000

    Time: 39 ms (46.73%) | Memory: 16.7 MB (43.85%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -3,7 +3,7 @@ import re
 class Solution:
     def simplifyPath(self, path: str) -> str:
         path = re.sub(r"\/+$", "", re.sub(r"\/+", "/", re.sub(r"(?<=\/)\.\/", "/", path)))
-        print(f"{path = }")
+        # print(f"{path = }")
         pathSplit = path.split("/")
         pathSplit1 = path.split("/")
         i = len(pathSplit) - 1
@@ -12,7 +12,7 @@ class Solution:
             if pathSplit[i] == ".":
                 del pathSplit1[i]
             if pathSplit[i] == "..":
-                print(f"{pathSplit1 = }")
+                # print(f"{pathSplit1 = }")
                 count += 1
                 while i > 0:
                     i -= 1
@@ -24,12 +24,12 @@ class Solution:
                 from_, to = i+count-1, i-count-1
                 for k in range(from_, i-1, -1):
                     if 0 <= k < len(pathSplit1):
-                        print(f"from {pathSplit1[k] = }")
+                        # print(f"from {pathSplit1[k] = }")
                         pathSplit1[k] = ""
                 for k in range(i-1, -1, -1):
                     if count > 0 and pathSplit1[k] != ".." and pathSplit1[k] != "":
                         count -= 1
-                        print(f"to {pathSplit1[k] = }, {count = }")
+                        # print(f"to {pathSplit1[k] = }, {count = }")
                         pathSplit1[k] = ""
             i -= 1
             
