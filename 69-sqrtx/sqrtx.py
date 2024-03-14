605b7683febd7957b798ca16672e00bbbfe4137a
Kang-Soo Lee <kipacti@gmail.com>
2024-03-14 01:55:53 +0000

    Time: 23 ms (99.65%) | Memory: 16.5 MB (91.00%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,18 +1,13 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        def findSqrt(left: int, right: int) -> int:
-            print(f"{left = }, {right = }")
-            if left >= right - 1:
-                return right - 1
-            mid = (left + right) // 2
-            if mid * mid == x:
-                return mid
-            elif mid * mid > x:
-                return findSqrt(left, mid)
+        l = 0
+        r = x
+        while l < r:
+            mid = l + (r - l + 1) // 2
+
+            if mid ** 2 <= x:
+                l = mid
             else:
-                return findSqrt(mid, right)
-        cand = findSqrt(0, x)
-        if (cand+1) * (cand+1) <= x:
-            return cand+1
-        else:
-            return cand
+                r = mid - 1
+
+        return l
