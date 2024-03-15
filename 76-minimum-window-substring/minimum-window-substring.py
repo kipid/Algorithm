bd49af0909993115ac5cd912ea21ac735a89e08f
Kang-Soo Lee <kipacti@gmail.com>
2024-03-15 08:09:29 +0000

    Time: 74 ms (95.76%) | Memory: 17.3 MB (41.50%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,34 +1,32 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        target_freq = {}  # Frequency of characters in string t
+        if not s or not t or len(s) < len(t):
+            return ""
+
+        map = [0] * 128
+        count = len(t)
+        start = 0
+        end = 0
+        min_len = float('inf')
+        start_index = 0
+        # UPVOTE !
         for char in t:
-            target_freq[char] = target_freq.get(char, 0) + 1
+            map[ord(char)] += 1
 
-        window_freq = {}  # Frequency of characters in the current window
-        count = 0  # Number of characters from t currently in the window
-        min_len = float('inf')  # Initialize with positive infinity
-        min_window = ""
+        while end < len(s):
+            if map[ord(s[end])] > 0:
+                count -= 1
+            map[ord(s[end])] -= 1
+            end += 1
 
-        left, right = 0, 0
-        while right < len(s):
-            # Expand the window
-            if s[right] in target_freq:
-                window_freq[s[right]] = window_freq.get(s[right], 0) + 1
-                if window_freq[s[right]] <= target_freq[s[right]]:
+            while count == 0:
+                if end - start < min_len:
+                    start_index = start
+                    min_len = end - start
+
+                if map[ord(s[start])] == 0:
                     count += 1
+                map[ord(s[start])] += 1
+                start += 1
 
-            # Contract the window
-            while count == len(t):
-                if right - left + 1 < min_len:
-                    min_len = right - left + 1
-                    min_window = s[left:right + 1]
-
-                if s[left] in target_freq:
-                    window_freq[s[left]] -= 1
-                    if window_freq[s[left]] < target_freq[s[left]]:
-                        count -= 1
-                left += 1
-
-            right += 1
-
-        return min_window
+        return "" if min_len == float('inf') else s[start_index:start_index + min_len]
