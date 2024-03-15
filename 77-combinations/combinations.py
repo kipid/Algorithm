21c3989c9a76692c4ebd4aa5e225bff5289f7400
Kang-Soo Lee <kipacti@gmail.com>
2024-03-15 23:57:27 +0000

    Time: 601 ms (96.09%) | Memory: 37.5 MB (98.32%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,14 +1,4 @@
+from itertools import combinations
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        # n, n-1, n-2, ..., n-k+1
-        res = []
-
-        def ithCombine(i: int, maxN: int, path: List[int]):
-            if i == 0:
-                res.append(path)
-                return
-            for j in range(maxN+1, n-i+2):
-                ithCombine(i-1, j, path + [j])
-        
-        ithCombine(k, 0, [])
-        return res
+        return combinations(range(1, n+1), k)
