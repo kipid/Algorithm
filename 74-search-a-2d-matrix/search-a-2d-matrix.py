e23bd3befab482dacb08e68a3b582f2e2298746c
Kang-Soo Lee <kipacti@gmail.com>
2024-03-15 00:02:46 +0000

    Time: 48 ms (42.29%) | Memory: 17.1 MB (38.16%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -9,7 +9,7 @@ class Solution:
             return False
         while left < right:
             mid = (left + right + 1) // 2
-            print(f"{left = }, {right = }, {mid = }")
+            # print(f"{left = }, {right = }, {mid = }")
             if matrix[mid // n][mid % n] <= target:
                 left = mid
             else:
