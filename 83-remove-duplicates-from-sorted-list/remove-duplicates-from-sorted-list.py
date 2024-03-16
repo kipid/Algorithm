7c993e742850d124282dccc0a0196c0669596051
Kang-Soo Lee <kipacti@gmail.com>
2024-03-16 10:44:00 +0000

    Time: 38 ms (72.61%) | Memory: 16.7 MB (7.26%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -5,10 +5,12 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        def delDup(head: Optional[ListNode]) -> Optional[ListNode]:
-            if head == None or head.next == None:
-                return None
-            last, curr, next = head, head, head.next
+        dummy = ListNode(next=head)
+        last, curr = dummy, head
+        while curr != None and (next := curr.next) != None:
+            # if curr == None or curr.next == None:
+            #     return None
+            # last, curr, next = head, head, head.next
             while next != None and curr.val != next.val:
                 last, curr, next = curr, next, next.next
             deleted = False
@@ -17,12 +19,9 @@ class Solution:
                 curr, next = next, next.next
             if deleted:
                 last.next = curr
-                return curr
+                if curr == None:
+                    break
+                last, curr = curr, curr.next
             else:
-                return None
-        
-        dummy = ListNode(-101, head)
-        curr = dummy
-        while (curr := delDup(curr)) != None:
-            pass
+                break
         return dummy.next
