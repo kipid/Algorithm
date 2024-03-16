69c537f3bb2dfbd19934e25c8f0dc5a2681201bb
Kang-Soo Lee <kipacti@gmail.com>
2024-03-16 07:11:12 +0000

    Time: 51 ms (67.64%) | Memory: 17.1 MB (59.63%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,23 +1,24 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        start = 0
-        n = len(nums)
-        end = n - 1
-
-        while start <= end:
-            mid = start + (end - start) // 2
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = (left + right + 1) // 2
             if nums[mid] == target:
                 return True
-            if nums[mid] == nums[end]:  # Fail to estimate which side is sorted
-                end -= 1
-            elif nums[mid] > nums[end]: # Left side is sorted
-                if nums[start] <= target and target < nums[mid]:    # Check if target in left side
-                    end = mid - 1
-                else:   # Otherwise check the unsorted side
-                    start = mid + 1
-            else:   # Right side is sorted
-                if nums[mid] < target and target <= nums[end]:      # Check if target is in right side
-                    start = mid + 1
+            if nums[mid] == nums[right]: # Fail to estimate which side is sorted
+                right -= 1
+                continue
+            if nums[left] == nums[mid]: # Fail to estimate which side is sorted
+                left += 1
+                continue
+            if nums[left] < nums[mid]: # Left side is sorted
+                if nums[left] <= target < nums[mid]: # Check if target in left side
+                    right = mid - 1
+                else: # Otherwise check the unsorted side
+                    left = mid
+            else: # Right side is sorted
+                if nums[mid] < target <= nums[right]: # Check if target is in right side
+                    left = mid + 1
                 else:
-                    end = mid - 1
-        return False
+                    right = mid
+        return nums[left] == target
