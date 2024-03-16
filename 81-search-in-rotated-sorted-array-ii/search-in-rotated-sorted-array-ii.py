c52971b16b75ea429de63b9e45e415fb981c59e5
Kang-Soo Lee <kipacti@gmail.com>
2024-03-16 06:44:42 +0000

    Time: 56 ms (32.50%) | Memory: 17.3 MB (22.19%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,4 +1,23 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        numsSet = set(nums)
-        return target in numsSet
+        start = 0
+        n = len(nums)
+        end = n - 1
+
+        while start <= end:
+            mid = start + (end - start) // 2
+            if nums[mid] == target:
+                return True
+            if nums[mid] == nums[end]:  # Fail to estimate which side is sorted
+                end -= 1
+            elif nums[mid] > nums[end]: # Left side is sorted
+                if nums[start] <= target and target < nums[mid]:    # Check if target in left side
+                    end = mid - 1
+                else:   # Otherwise check the unsorted side
+                    start = mid + 1
+            else:   # Right side is sorted
+                if nums[mid] < target and target <= nums[end]:      # Check if target is in right side
+                    start = mid + 1
+                else:
+                    end = mid - 1
+        return False
