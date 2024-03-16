678c4325f95a51f37e71f537a3c4f37215c1895a
Kang-Soo Lee <kipacti@gmail.com>
2024-03-16 07:38:20 +0000

    Time: 45 ms (47.05%) | Memory: 17.1 MB (21.83%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -2,23 +2,20 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
     
-        while left <= right:
-            mid = (right + left) // 2
-            
+        while left < right:
+            mid = (right + left + 1) // 2
+            print(f"{left = }, {mid = }, {right = }")
             if nums[mid] == target:
                 return mid
-            
-            # 왼쪽 부분이 정렬되어 있는 경우
-            if nums[left] <= nums[mid]:
+            if nums[left] < nums[mid]: # 왼쪽 부분이 정렬되어 있는 경우
                 if nums[left] <= target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
-            # 오른쪽 부분이 정렬되어 있는 경우
-            else:
+            else: # 오른쪽 부분이 정렬되어 있는 경우
                 if nums[mid] < target <= nums[right]:
                     left = mid + 1
                 else:
                     right = mid - 1
-        
-        return -1
+        print(f"{left = }")
+        return left if nums[left%len(nums)] == target else -1
