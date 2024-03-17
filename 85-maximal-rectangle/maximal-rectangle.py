2b7bf8335aca43cdfe17a823012d34a7016d0f14
Kang-Soo Lee <kipacti@gmail.com>
2024-03-17 09:02:50 +0000

    Time: 211 ms (85.56%) | Memory: 17.8 MB (80.44%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.py
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -1,43 +1,19 @@
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        m, n = len(matrix), len(matrix[0])
-        maxArea = 0
-
-        def isRect(row: int, row1: int, col:int, col1:int) -> bool:
-            for r in range(row+1, row1):
-                for c in range(col+1, col1):
-                    if matrix[r][c] == "0":
-                        return False
-            return True
-
-        def maxAreaRect(row: int, col: int) -> int:
-            maxArea = 1
-            row1 = row + 1
-            while row1 < m and matrix[row1][col] == "1":
-                row1 += 1
-            maxArea = max(maxArea, row1-row)
-            col1 = col + 1
-            while col1 < n and matrix[row][col1] == "1":
-                col1 += 1
-            maxArea = max(maxArea, col1-col)
-            if row+1 < m and col+1 < n and row1 > row+1 and col1 > col+1 and matrix[row+1][col+1] == "1":
-                isFirstAndMaxArea = False
-                r = row1
-                c = col1
-                if isRect(row, r, col, c):
-                    return max(maxArea, (r-row)*(c-col))
-                for r in range(row1, row, -1):
-                    for c in range(col1, col, -1):
-                        if maxArea < (r-row)*(c-col) and isRect(row, r, col, c):
-                            maxArea = max(maxArea, (r-row)*(c-col))
-            return maxArea
-
-        if matrix[0][0] == "1":
-            maxArea = max(maxArea, maxAreaRect(0, 0))
-        if maxArea == m * n:
-            return maxArea
-        for row in range(m):
-            for col in range(n):
-                if matrix[row][col] == "1":
-                    maxArea = max(maxArea, maxAreaRect(row, col))
-        return maxArea
+        maxarea = 0
+        dp = [0] * len(matrix[0])
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                dp[j] = dp[j] + 1 if matrix[i][j] == '1' else 0
+            maxarea = max(maxarea, self.maxArea(dp))
+        return maxarea
+    def maxArea(self, heights):
+        stack = [-1]
+        maxarea = 0
+        for i in range(len(heights)):
+            while stack[-1] != -1 and heights[stack[-1]] >= heights[i]:
+                maxarea = max(maxarea, heights[stack.pop()] * (i - stack[-1] - 1))
+            stack.append(i)
+        while stack[-1] != -1:
+            maxarea = max(maxarea, heights[stack.pop()] * (len(heights) - stack[-1] - 1))
+        return maxarea
