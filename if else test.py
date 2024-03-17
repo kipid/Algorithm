6014cba031a10f1e93a23aa454f2d72c5077ad87
Kang-Soo Lee <kipacti@gmail.com>
2024-03-17 15:15:35 +0900

    Create if else test.py

---
+++ if else test.py
@@ -0,0 +1,41 @@
+if []:
+	print("True")
+else:
+	print("False")
+
+if not []:
+	print("True")
+else:
+	print("False")
+
+if "":
+	print("True")
+else:
+	print("False")
+
+if {}:
+	print("True")
+else:
+	print("False")
+
+if ():
+	print("True")
+else:
+	print("False")
+
+tuple_ = ()
+
+if tuple_:
+	print("True")
+else:
+	print("False")
+
+if set():
+	print("True")
+else:
+	print("False")
+
+if list():
+	print("True")
+else:
+	print("False")
