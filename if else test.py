63ce6d588ec8c33a57945111132e9d070d1fa20c
Kang-Soo Lee <kipacti@gmail.com>
2024-03-17 17:40:09 +0900

    Update if else test.py

--- if else test.py
+++ if else test.py
@@ -39,3 +39,23 @@ if list():
 	print("True")
 else:
 	print("False")
+
+if 0:
+	print("True")
+else:
+	print("False")
+
+if 1:
+	print("True")
+else:
+	print("False")
+
+if "0":
+	print("True")
+else:
+	print("False")
+
+if "1":
+	print("True")
+else:
+	print("False")
