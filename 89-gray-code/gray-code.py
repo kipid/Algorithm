232c3936e6322e709a2d0de863ed96556a0b9cb0
Kang-Soo Lee <kipacti@gmail.com>
2024-03-18 06:51:35 +0000

    Time: 72 ms (65.67%) | Memory: 23.9 MB (55.71%) - LeetSync

---
+++ 89-gray-code/gray-code.py
@@ -0,0 +1,13 @@
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        if n == 1:
+            return [0,1]
+        minus1 = self.grayCode(n-1)
+        highestBit = 2**(n-1)
+        return minus1 + [highestBit + minus1[i] for i in range(len(minus1)-1, -1, -1)]
+        # [000,001,011,010
+        # ,110,111,101,100]
+        # [0000,0001,0011,0010
+        # ,0110,0111,0101,0100
+        # ,1100,1101,1111,1110
+        # ,1010,1011,1001,1000]
