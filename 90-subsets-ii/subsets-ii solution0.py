734b3e133e982df667c1a50f074f712a08389348
Kang-Soo Lee <kipacti@gmail.com>
2024-03-18 18:48:11 +0900

    Create subsets-ii solution0.py

---
+++ 90-subsets-ii/subsets-ii solution0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = {()}
+        dict_ = dict()
+        for num in nums:
+            dict_[num] = dict_.get(num, 0) + 1
+        for key, freq in dict_.items():
+            res1 = set(res)
+            for item in res1:
+                res.update([item + (key,) * k for k in range(freq+1)])
+        res2 = []
+        for item in res:
+            res2.append(list(item))
+        return res
