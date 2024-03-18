f694fe76cf955d3e576abd3115e20a29c8d5847b
Kang-Soo Lee <kipacti@gmail.com>
2024-03-18 09:47:50 +0000

    Time: 45 ms (22.51%) | Memory: 16.6 MB (90.60%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,14 +1,15 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        res = {()}
-        dict_ = dict()
-        for num in nums:
-            dict_[num] = dict_.get(num, 0) + 1
-        for key, freq in dict_.items():
-            res1 = set(res)
-            for item in res1:
-                res.update([item + (key,) * k for k in range(freq+1)])
-        res2 = []
-        for item in res:
-            res2.append(list(item))
-        return res
+        subsets = []
+        nums.sort()
+
+        def backtracking(current: List[int], k: int=0):
+            subsets.append(current)
+
+            for i in range(k, len(nums)):
+                if i > k and nums[i] == nums[i-1]:
+                    continue
+                backtracking(current + [nums[i]], i+1)
+
+        backtracking([])
+        return subsets
