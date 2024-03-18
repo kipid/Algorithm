f64a7a16c6e9fb814f46f474ecafce8007764860
Kang-Soo Lee <kipacti@gmail.com>
2024-03-18 09:39:41 +0000

    Time: 45 ms (22.51%) | Memory: 16.9 MB (9.37%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -6,9 +6,8 @@ class Solution:
             dict_[num] = dict_.get(num, 0) + 1
         for key, freq in dict_.items():
             res1 = set(res)
-            for k in range(freq+1):
-                for item in res1:
-                    res.add(item + (key,) * k)
+            for item in res1:
+                res.update([item + (key,) * k for k in range(freq+1)])
         res2 = []
         for item in res:
             res2.append(list(item))
