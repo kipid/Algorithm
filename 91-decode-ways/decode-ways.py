26f3cf90883a20304ab2f3f59836a97da9ffee68
Kang-Soo Lee <kipacti@gmail.com>
2024-03-19 01:52:28 +0000

    Time: 31 ms (87.13%) | Memory: 16.6 MB (35.67%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,11 +1,10 @@
 import re
 
-class Solution:
-    Fibonacci = [1, 1]
-    def __init__(self):
-        for i in range(2, 101):
-            self.Fibonacci.append(self.Fibonacci[-2]+self.Fibonacci[-1])
+Fibonacci = [1, 1]
+for i in range(2, 101):
+    Fibonacci.append(Fibonacci[-2]+Fibonacci[-1])
 
+class Solution:
     def numDecodings(self, s: str) -> int:
         if re.search(r"00+", s):
             return 0
@@ -45,7 +44,7 @@ class Solution:
                             lastStrIndex += 1 + len(s2) # split 포함 indexing
                             length = len(s2) # 1 0, 112 0 121221 0 12 0 (0을 바로 전과 묶음)
                             if i2 != len(splitBy0)-1:
-                                res *= self.Fibonacci[length-1]
+                                res *= Fibonacci[length-1]
                         if len(splitBy0[-1]) > 0 and splitBy0[-1][-1] != "" and lastStrIndex < len(s): # 1 0 2 ("1" or "2")
                         # no and i1 != len(splitAbove2)-1
                             if splitBy0[-1][-1] == "1":
@@ -53,15 +52,9 @@ class Solution:
                             elif int(s[lastStrIndex]) < 7: # splitBy0[-1][-1] == "2"
                                 length += 1
                         print(f"{length = }")
-                        res *= self.Fibonacci[length]
+                        res *= Fibonacci[length]
                     else:
                         lastStrIndex += 1
-                # if len(splitAbove2[-1]) > 0 and splitAbove2[-1][-1] != "" and lastStrIndex < len(s): # 23 7 612342111 9 8 65 7
-                #     if splitAbove2[-1][-1] == "1":
-                #         length += 1
-                #     elif splitAbove2[-1][-1] == "2" and int(s[lastStrIndex]) < 7:
-                #         length += 1
-                # res *= counting(length - 1)
             else:
                 lastStrIndex += 1
         return res
