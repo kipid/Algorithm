414bc76d3f0e03e51e5faf81c07c2b0f11576498
Kang-Soo Lee <kipacti@gmail.com>
2024-03-19 03:36:23 +0000

    Time: 40 ms (29.88%) | Memory: 16.6 MB (35.67%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -3,11 +3,11 @@ class Solution:
         if s[0] == "0":
             return 0
         dp = [1,1]
-        for ind, num in enumerate(s[1:],2):
+        for ith, num in enumerate(s[1:],2):
             ways = 0
             if num != "0":
-                ways += dp[ind-1]
-            if 10 <= int(s[ind-2] + num) <= 26:
-                ways += dp[ind-2]
+                ways += dp[ith-1]
+            if 10 <= int(s[ith-2] + num) <= 26:
+                ways += dp[ith-2]
             dp.append(ways)
         return dp[-1]
