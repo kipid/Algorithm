5d6a5a1211445da1eca0f055fd7add10ecc1a8ea
Kang-Soo Lee <kipacti@gmail.com>
2024-03-20 02:05:16 +0000

    Time: 53 ms (26.76%) | Memory: 18.3 MB (7.62%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
@@ -6,24 +6,15 @@
 #         self.right = right
 class Solution:
     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
-        # using explicit memoization (not using lru_cache), just recursion. we can split into left trees and right trees.
-        # we pick a node, the numbers on the left will make left trees, the numbers on the
-        # right will make right trees.
-        # we use memoization to store the intermediate results
-        dp = defaultdict(list)
-        def helper(left, right):    # construct trees using left->right numbers
-            if left > right:
+        def dfs(begin: int, end: int) -> list[TreeNode | None]:
+            if begin == end:
                 return [None]
-            if (left, right) in dp:
-                return dp[(left, right)]
-            for i in range(left, right+1):  # i can choose right, so the range is to right+1
-                lefts = helper(left, i-1)
-                rights = helper(i+1, right)
-                for l in lefts:     # go through all the lefts and rights
-                    for r in rights:
-                        root = TreeNode(i)  # root created
-                        root.left = l
-                        root.right = r
-                        dp[(left, right)].append(root)
-            return dp[(left, right)]    # just return the stored values
-        return helper(1, n)
+
+            return [
+                TreeNode(root, left=left, right=right)
+                for root in range(begin, end)
+                for left in dfs(begin, root)
+                for right in dfs(root + 1, end)
+            ]
+        
+        return dfs(1, n + 1)
