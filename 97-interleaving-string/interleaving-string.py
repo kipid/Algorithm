91a73fca9ef4b01097149b1dbb15cd28748e3c05
Kang-Soo Lee <kipacti@gmail.com>
2024-03-21 00:03:22 +0000

    Time: 31 ms (96.91%) | Memory: 17.3 MB (29.87%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -4,7 +4,7 @@ class Solution:
         def rec(i1, i2, i3):
             if i1 == len(s1) and i2 == len(s2) and i3 == len(s3):
                 return True
-            return i3 < len(s3) and (i1 < len(s1) and s1[i1] == s3[i3] and rec(i1+1, i2, i3+1) or i2 < len(s2) and s2[i2] == s3[i3] and rec(i1, i2+1, i3+1))
+            return i3 < len(s3) and (i1 < len(s1) and s1[i1] == s3[i3] and rec(i1+1, i2, i3+1)) or (i2 < len(s2) and s2[i2] == s3[i3] and rec(i1, i2+1, i3+1))
         if len(s3) != len(s1) + len(s2):
             return False
         return rec(0, 0, 0)
