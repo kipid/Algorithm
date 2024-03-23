12558b8af1788558acd39e20fe85f93a9fdf1bec
Kang-Soo Lee <kipacti@gmail.com>
2024-03-23 18:07:45 +0900

    Create Binary Search Tree (BST).py

---
+++ Binary Search Tree (BST).py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        print(root.val, end=" ")
+        inorder_traversal(root.right)
+
+def preorder_traversal(root):
+    if root:
+        print(root.val, end=" ")
+        preorder_traversal(root.left)
+        preorder_traversal(root.right)
+
+def postorder_traversal(root):
+    if root:
+        postorder_traversal(root.left)
+        postorder_traversal(root.right)
+        print(root.val, end=" ")
+
+root = TreeNode(5)
+root.left = TreeNode(3)
+root.right = TreeNode(8)
+root.left.left = TreeNode(2)
+root.left.right = TreeNode(4)
+
+inorder_traversal(root) # 2 3 4 5 8
+print()
+preorder_traversal(root) # 5 3 2 4 8
+print()
+postorder_traversal(root) # 2 4 3 8 5
+print()
