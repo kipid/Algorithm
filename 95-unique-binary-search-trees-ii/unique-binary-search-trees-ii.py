d1c4915438448fbbf0dcca4b790ca0f47f8e5ddb
Kang-Soo Lee <kipacti@gmail.com>
2024-03-23 09:27:58 +0000

    Time: 41 ms (89.85%) | Memory: 18.3 MB (7.64%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
@@ -4,21 +4,17 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-memos = defaultdict(list)
-def helper(left, right):    # construct trees using left->right numbers
-    if left > right:
-        return [None]
-    if (left, right) in memos:
-        return memos[(left, right)]
-    for i in range(left, right+1):  # i can choose right, so the range is to right+1
-        lefts = helper(left, i-1)
-        rights = helper(i+1, right)
-        for l in lefts:     # go through all the lefts and rights
-            for r in rights:
-                root = TreeNode(i, l, r)  # root created
-                memos[(left, right)].append(root)
-    return memos[(left, right)]    # just return the stored values
-helper(1, 8)
 class Solution:
-    def generateTrees(self, n: int) -> list[Optional[TreeNode]]:
-        return helper(1, n)
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        def buildTree(begin: int, end: int) -> list[TreeNode | None]:
+            if begin == end:
+                return [None]
+
+            return [
+                TreeNode(root, left, right)
+                for root in range(begin, end)
+                    for left in buildTree(begin, root)
+                        for right in buildTree(root + 1, end)
+            ]
+        
+        return buildTree(1, n + 1)
