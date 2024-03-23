7f865550a1d7807ea99fb65bf79cc3186f1ab7e4
Kang-Soo Lee <kipacti@gmail.com>
2024-03-23 05:34:50 +0000

    Time: 56 ms (61.65%) | Memory: 51.4 MB (70.68%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+    let res = []
+
+    let inorder = function (root) {
+        if (!root) {
+            return
+        }
+        inorder(root.left);
+        res.push(root.val);
+        inorder(root.right);
+    };
+    
+    inorder(root)
+    return res
+};
