f87be4fa2b19fc7fbc2f1afa9d4b34a17973fdf4
Kang-Soo Lee <kipacti@gmail.com>
2024-03-23 09:51:37 +0000

    Time: 33 ms (68.95%) | Memory: 16.6 MB (5.89%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/unique-binary-search-trees.py
@@ -1,12 +1,9 @@
-memo = {0:1, 1:1}
 class Solution:
+    @lru_cache
     def numTrees(self, n: int) -> int:
         if n<=1:
             return 1
         res = 0
-        if n in memo:
-            return memo[n]
         for i in range(n):
             res += self.numTrees(i) * self.numTrees(n-1-i)
-        memo[n] = res
         return res
