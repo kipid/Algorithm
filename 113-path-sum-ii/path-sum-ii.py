b650fe897162ec4198c05f7395316e548a646558
Kang-Soo Lee <kipacti@gmail.com>
2024-03-23 03:14:52 +0000

    Time: 42 ms (60.74%) | Memory: 21 MB (36.04%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.py
+++ 113-path-sum-ii/path-sum-ii.py
@@ -12,9 +12,8 @@ class Solution:
         def rootToLeaf(node: TreeNode, sumUntil: int, path: List[int]):
             sumUntil += node.val
             path = path + [node.val]
-            if not node.left and not node.right:
-                if sumUntil == targetSum:
-                    res.append(path)
+            if not node.left and not node.right and sumUntil == targetSum:
+                res.append(path)
             if node.left:
                 rootToLeaf(node.left, sumUntil, path)
             if node.right:
