d26a796c478bade1d762279f4a4fd7ba71bdda66
Kang-Soo Lee <kipacti@gmail.com>
2024-03-24 00:30:44 +0000

    Time: 48 ms (93.11%) | Memory: 18.3 MB (76.13%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,33 +6,25 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        # Create a hashmap to store the indices of elements in the inorder list
-        inorder_map = {val: index for index, val in enumerate(inorder)}
+        inorder_map = {} # v -> inorder index
+        for i, v in enumerate(inorder):
+            inorder_map[v] = i
 
-        # Define a recursive function to build the binary tree
-        def build(pre_start: int, pre_end: int, in_start: int, in_end: int) -> Optional[TreeNode]:
-            # Base case: If the start index in the preorder list is greater than the end index,
-            # it indicates that the current subtree is null, so return None
-            if pre_start > pre_end:
+        curr_node_preorder_index = 0
+
+        def buildTree(left, right):
+            if left > right:
                 return None
+            
+            nonlocal curr_node_preorder_index
+            curr_node_value = preorder[curr_node_preorder_index]
+            curr_node_inorder_index = inorder_map[curr_node_value]
 
-            # Extract the value of the root node from the preorder list using the start index
-            root_val = preorder[pre_start]
-            root = TreeNode(root_val)
+            curr_node = TreeNode(curr_node_value)
+            curr_node_preorder_index += 1
 
-            # Find the index of the root node's value in the inorder list using the hashmap
-            in_index = inorder_map[root_val]
+            curr_node.left = buildTree(left, curr_node_inorder_index-1)
+            curr_node.right = buildTree(curr_node_inorder_index+1, right)
+            return curr_node
 
-            # Determine the size of the left subtree
-            left_size = in_index - in_start
-
-            # Recursively build the left subtree
-            root.left = build(pre_start + 1, pre_start + left_size, in_start, in_index - 1)
-
-            # Recursively build the right subtree
-            root.right = build(pre_start + left_size + 1, pre_end, in_index + 1, in_end)
-
-            return root
-
-        # Call the build function with initial parameters corresponding to the entire preorder and inorder lists
-        return build(0, len(preorder) - 1, 0, len(inorder) - 1)
+        return buildTree(0, len(inorder)-1)
