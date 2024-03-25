c89851a738222f7856a323a6917df9aeb3e80a0b
Kang-Soo Lee <kipacti@gmail.com>
2024-03-25 08:41:09 +0000

    Time: 1357 ms (5.02%) | Memory: 17.5 MB (87.14%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,20 +1,17 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        cache = {}
-
+        @lru_cache
         def dfs(i,j):
             if j == len(t):
                 return 1
             if i == len(s):
                 return 0
-            if (i,j) in cache:
-                return cache[(i,j)]
             if len(s) - i < len(t) - j:
                 return 0
 
             if s[i] == t[j]:
-                cache[(i,j)] = dfs(i+1,j+1)+dfs(i+1,j)
+                return dfs(i+1,j+1)+dfs(i+1,j)
             else:
-                cache[(i,j)] = dfs(i+1,j)
-            return cache[(i,j)]
+                return dfs(i+1,j)
+
         return dfs(0,0)
