8af70c8680258a3c9f3af5dbe1b1946498a2db64
Kang-Soo Lee <kipacti@gmail.com>
2024-03-25 08:46:28 +0000

    Time: 53 ms (94.50%) | Memory: 20.4 MB (85.92%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,17 +1,22 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        @lru_cache
-        def dfs(i,j):
-            if j == len(t):
-                return 1
-            if i == len(s):
+        memo = {}
+        def count(sIndex: int, tIndex: int) -> int:
+            if sIndex < tIndex:
                 return 0
-            if len(s) - i < len(t) - j:
-                return 0
-
-            if s[i] == t[j]:
-                return dfs(i+1,j+1)+dfs(i+1,j)
+            if (sIndex, tIndex) in memo:
+                return memo[(sIndex, tIndex)]
+            if sIndex == 0 and tIndex == 0:
+                memo[(0, 0)] = 1 if s[0]==t[0] else 0
+                return memo[(0, 0)]
+            if sIndex > 0 and tIndex == 0:
+                memo[(sIndex, tIndex)] = count(sIndex-1, 0) + 1 if s[sIndex]==t[0] else count(sIndex-1, 0)
+                return memo[(sIndex, tIndex)]
+            if s[sIndex] == t[tIndex]:
+                memo[(sIndex, tIndex)] = count(sIndex-1, tIndex-1) + count(sIndex-1, tIndex)
+                return memo[(sIndex, tIndex)]
             else:
-                return dfs(i+1,j)
+                memo[(sIndex, tIndex)] = count(sIndex-1, tIndex)
+                return memo[(sIndex, tIndex)]
 
-        return dfs(0,0)
+        return count(len(s)-1, len(t)-1)
