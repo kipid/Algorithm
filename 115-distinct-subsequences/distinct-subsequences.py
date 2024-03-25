5b88272add5bafe0159ecd9cb8dc04154b0a3dc5
Kang-Soo Lee <kipacti@gmail.com>
2024-03-25 08:22:43 +0000

    Time: 43 ms (98.97%) | Memory: 20.3 MB (86.11%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,18 +1,20 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        @lru_cache
-        def count(sIndex: int, tIndex: int) -> int:
-            if sIndex < tIndex:
-                return 0
-            if sIndex == 0 and tIndex == 0:
-                return 1 if s[0]==t[0] else 0
-            if sIndex > 0 and tIndex == 0:
-                return count(sIndex-1, 0) + 1 if s[sIndex]==t[0] else count(sIndex-1, 0)
-            # if sIndex < 0 or tIndex < 0:
-            #     return 0
-            if s[sIndex] == t[tIndex]:
-                return count(sIndex-1, tIndex-1) + count(sIndex-1, tIndex)
-            else:
-                return count(sIndex-1, tIndex)
+        cache = {}
 
-        return count(len(s)-1, len(t)-1)
+        def dfs(i,j):
+            if j == len(t):
+                return 1
+            if i == len(s):
+                return 0
+            if (i,j) in cache:
+                return cache[(i,j)]
+            if len(s) - i < len(t) - j:
+                return 0
+
+            if s[i] == t[j]:
+                cache[(i,j)] = dfs(i+1,j+1)+dfs(i+1,j)
+            else:
+                cache[(i,j)] = dfs(i+1,j)
+            return cache[(i,j)]
+        return dfs(0,0)
