d65d323829838c5753c348107a534679827faf64
Kang-Soo Lee <kipacti@gmail.com>
2024-03-25 08:48:21 +0000

    Time: 39 ms (99.51%) | Memory: 20.2 MB (86.33%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -6,12 +6,13 @@ class Solution:
                 return 0
             if (sIndex, tIndex) in memo:
                 return memo[(sIndex, tIndex)]
-            if sIndex == 0 and tIndex == 0:
-                memo[(0, 0)] = 1 if s[0]==t[0] else 0
-                return memo[(0, 0)]
-            if sIndex > 0 and tIndex == 0:
-                memo[(sIndex, tIndex)] = count(sIndex-1, 0) + 1 if s[sIndex]==t[0] else count(sIndex-1, 0)
-                return memo[(sIndex, tIndex)]
+            if tIndex == 0:
+                if sIndex == 0:
+                    memo[(0, 0)] = 1 if s[0]==t[0] else 0
+                    return memo[(0, 0)]
+                if sIndex > 0:
+                    memo[(sIndex, tIndex)] = count(sIndex-1, 0) + 1 if s[sIndex]==t[0] else count(sIndex-1, 0)
+                    return memo[(sIndex, tIndex)]
             if s[sIndex] == t[tIndex]:
                 memo[(sIndex, tIndex)] = count(sIndex-1, tIndex-1) + count(sIndex-1, tIndex)
                 return memo[(sIndex, tIndex)]
