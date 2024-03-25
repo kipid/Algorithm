1d49bb8ccaf7dc0fcf82878a124023f860c876bc
Kang-Soo Lee <kipacti@gmail.com>
2024-03-25 08:19:19 +0000

    Time: 688 ms (5.70%) | Memory: 17.5 MB (87.14%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -2,14 +2,14 @@ class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         @lru_cache
         def count(sIndex: int, tIndex: int) -> int:
-            if sIndex < 0 or tIndex < 0:
-                return 0
             if sIndex < tIndex:
                 return 0
             if sIndex == 0 and tIndex == 0:
                 return 1 if s[0]==t[0] else 0
             if sIndex > 0 and tIndex == 0:
                 return count(sIndex-1, 0) + 1 if s[sIndex]==t[0] else count(sIndex-1, 0)
+            # if sIndex < 0 or tIndex < 0:
+            #     return 0
             if s[sIndex] == t[tIndex]:
                 return count(sIndex-1, tIndex-1) + count(sIndex-1, tIndex)
             else:
