6c7f0ad69b3ff3716f5e6e22eb1779d8e26ababe
Kang-Soo Lee <kipacti@gmail.com>
2024-03-27 08:45:14 +0000

    Time: 904 ms (5.01%) | Memory: 28.7 MB (11.64%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,9 +4,9 @@ class Solution:
         minPriceUntil = [prices[0]]
         for i in range(1, len(prices)-1):
             minPriceUntil.append(min(minPriceUntil[-1], prices[i]))
-        maxPriceAfter = [prices[-1]]
+        maxPriceAfter = prices[-1]
         profit = 0
         for i in range(len(prices)-2, -1, -1):
-            profit = max(profit, -minPriceUntil[i]+maxPriceAfter[-1])
-            maxPriceAfter.append(max(maxPriceAfter[-1], prices[i]))
+            profit = max(profit, -minPriceUntil[i]+maxPriceAfter)
+            maxPriceAfter = max(maxPriceAfter, prices[i])
         return profit
