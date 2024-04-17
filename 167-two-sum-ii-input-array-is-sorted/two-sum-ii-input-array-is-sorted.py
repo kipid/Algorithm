d35d574b4b462d907ccf7deeb66509782f0b26b1
Kang-Soo Lee <kipacti@gmail.com>
2024-04-17 17:30:07 +0900

    Time: 111 ms (26.33%) | Memory: 17.4 MB (57.69%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,10 +1,13 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        dictNums = dict()
-        for i in range(len(numbers)):
-            dictNums[numbers[i]] = i
-        for i in range(len(numbers)):
-            if (target - numbers[i]) < numbers[i]:
-                return
-            if (target - numbers[i]) in dictNums:
-                return [i+1, dictNums[target - numbers[i]]+1]
+        i = 0
+        j = len(numbers) - 1
+
+        # If current sum is too small, move closer to the right
+        # If current sum is too large, move closer to the left
+        while numbers[i] + numbers[j] != target:
+            if numbers[i] + numbers[j] < target:
+                i += 1
+            elif numbers[i] + numbers[j] > target:
+                j -= 1
+        return [i + 1, j + 1]
