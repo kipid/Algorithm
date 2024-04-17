d56f1f67e36a464d149d075347b5458c9d518006
Kang-Soo Lee <kipacti@gmail.com>
2024-04-17 10:08:34 +0900

    Time: 26 ms (98.10%) | Memory: 16.6 MB (46.77%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
@@ -1,9 +1,7 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        zeroes = 0
-        k = 1
-        while n >= (fives := 5**k):
-            for i in range(fives, n+1, fives):
-                zeroes += 1
-            k += 1
-        return zeroes
+        cnt=0
+        while n:
+            n//=5
+            cnt+=n
+        return cnt
