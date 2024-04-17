558eab2ecbef786523612a2b54c45e677d804e23
Kang-Soo Lee <kipacti@gmail.com>
2024-04-17 09:45:04 +0900

    Create majority-element solution1.py

---
+++ 169-majority-element/majority-element solution1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        elem = 0
+        for num in nums:
+            if count == 0:
+                elem = num
+                count += 1
+                continue
+            if elem == num:
+                count += 1
+            else:
+                count -= 1
+        return elem
