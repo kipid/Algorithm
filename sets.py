c851f3f40f003ded547fea8f1557b029f1ac602a
Kang-Soo Lee <kipacti@gmail.com>
2024-04-17 15:24:24 +0900

    Update sets.py

--- sets.py
+++ sets.py
@@ -1,4 +1,5 @@
 s = set()
+print(f"{s.add('1')}") # None
 s.add("1")
 s.add("1")
 s.add("1")
@@ -18,3 +19,4 @@ s = set(["1", "2", "4", "7"])
 print(s)
 s = set("123456789")
 print(s)
+print(f"{list(s)[0]}")
