6a76df14c3ebb395bdc670dbca3abb988867e531
Kang-Soo Lee <kipacti@gmail.com>
2024-04-18 10:29:41 +0900

    Time: 85 ms (30.88%) | Memory: 18.2 MB (24.94%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -7,7 +7,10 @@ class Solution:
             digit = 0
             sign = 1
             while k < len(s):
-                if s[k] == "(":
+                if s[k] == " ":
+                    k += 1
+                    continue
+                elif s[k] == "(":
                     subVal, k = evaluate(k+1)
                     curVal += sign * subVal
                 elif s[k] == ")":
