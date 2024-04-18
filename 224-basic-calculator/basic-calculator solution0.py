bc1dda698d08325c4935c6a70723eecbcfb582ef
Kang-Soo Lee <kipacti@gmail.com>
2024-04-18 10:33:19 +0900

    Create basic-calculator solution0.py

---
+++ 224-basic-calculator/basic-calculator solution0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        def evaluate(k):
+            curVal = 0
+            digit = 0
+            sign = 1
+            while k < len(s):
+                if s[k] == " ":
+                    k += 1
+                    continue
+                elif s[k] == "(":
+                    subVal, k = evaluate(k+1)
+                    curVal += sign * subVal
+                elif s[k] == ")":
+                    curVal += sign*digit
+                    return curVal, k
+                elif s[k] == "-":
+                    curVal += sign * digit
+                    digit = 0
+                    sign = -1
+                elif s[k] == "+":
+                    curVal += sign * digit
+                    digit = 0
+                    sign = 1
+                else:
+                    digit = digit * 10 + int(s[k])
+                k += 1
+            return curVal + sign * digit
+        return evaluate(0)
