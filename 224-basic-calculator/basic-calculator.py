58c0198b693adca2e6c2908114b2e8ac99a47b4c
Kang-Soo Lee <kipacti@gmail.com>
2024-04-18 10:25:45 +0900

    Time: 82 ms (34.39%) | Memory: 18.2 MB (40.49%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.py
@@ -0,0 +1,28 @@
+import re
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        def evaluate(k):
+            curVal = 0
+            digit = 0
+            sign = 1
+            while k < len(s):
+                if s[k] == "(":
+                    subVal, k = evaluate(k+1)
+                    curVal += sign * subVal
+                elif s[k] == ")":
+                    curVal += sign*digit
+                    return curVal, k
+                elif s[k] == "-":
+                    curVal += sign * digit
+                    digit = 0
+                    sign = -1
+                elif s[k] == "+":
+                    curVal += sign * digit
+                    digit = 0
+                    sign = 1
+                elif s[k].isdigit():
+                    digit = digit * 10 + int(s[k])
+                k += 1
+            return curVal + sign * digit
+        return evaluate(0)
