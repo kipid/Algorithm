8815f0cd78f4eabef675d47fd1c18d3eda860034
Kang-Soo Lee <kipacti@gmail.com>
2024-04-20 18:23:16 +0900

    Time: 112 ms (84.28%) | Memory: 28.7 MB (91.50%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -4,34 +4,28 @@ class Trie:
         self.root={}
         
     def insert(self, word: str) -> None:
-
         cur=self.root
-
         for letter in word:
             if letter not in cur:
                 cur[letter]={}
             cur=cur[letter]
-
         cur['*']=''
+        # print(f"{self.root = }")
 
     def search(self, word: str) -> bool:
-
         cur=self.root
         for letter in word:
             if letter not in cur:
                 return False
             cur=cur[letter]
-
         return '*' in cur
         
     def startsWith(self, prefix: str) -> bool:
-
         cur=self.root
         for letter in prefix:
             if letter not in cur:
                 return False
             cur=cur[letter]
-
         return True
 
 
