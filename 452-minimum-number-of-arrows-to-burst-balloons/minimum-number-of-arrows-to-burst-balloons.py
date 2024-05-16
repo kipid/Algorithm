a56e65b42e0f443cfe9b22f1bedad97b26bf00e6
Kang-Soo Lee <kipacti@gmail.com>
2024-05-16 12:42:12 +0900

    Time: 1095 ms (36.12%) | Memory: 62.8 MB (62.16%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -2,6 +2,7 @@ class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         arrows = 1
         points.sort(key = lambda x:x[1])
+        print(f"{points = }")
         first_end = points[0][1]
         # this is to count non-overlapping intervals:
         # so it doesn't matter if you use points[1:] or points
