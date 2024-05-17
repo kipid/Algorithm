65e109473f758b7e92e1bfb7db54c78aeb49015d
Kang-Soo Lee <kipacti@gmail.com>
2024-05-18 08:01:21 +0900

    Time: 54 ms (68.67%) | Memory: 21.8 MB (64.73%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.py
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.py
@@ -6,16 +6,10 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        
-        count = 0
-        def inorderCount(root: Optional[TreeNode]):
-            if not root:
-                return
-            inorderCount(root.left)
-            nonlocal count
-            count += 1
-            inorderCount(root.right)
-        inorderCount(root)
-        return count
+        def dfs(node):
+            if not node:
+                return 0
+            l=dfs(node.left)
+            r=dfs(node.right)
+            return l+r+1
+        return dfs(root)
