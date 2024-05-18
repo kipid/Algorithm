bada199f435ab3b9d351bda7750603063a19a693
Kang-Soo Lee <kipacti@gmail.com>
2024-05-19 07:31:07 +0900

    Time: 351 ms (89.60%) | Memory: 38.6 MB (24.57%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,37 +1,22 @@
 class MedianFinder:
 
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        # Initialize two heaps: small to store the smaller half as a max heap
-        # and large to store the larger half as a min heap
-        self.small, self.large = [], []
+        self.small = [] # smaller half of the list, max heap
+        self.large = [] # larger half of the list, min heap
+        
 
     def addNum(self, num: int) -> None:
-        # If large heap is not empty and the number is greater than the smallest number in large heap,
-        # push it to the large heap. Otherwise, push the negation of the number to the small heap.
-        if self.large and num > self.large[0]:
-            heapq.heappush(self.large, num)
+        if len(self.small) == len(self.large):
+            heappush(self.large, -heappushpop(self.small, -num))
         else:
-            heapq.heappush(self.small, -1 * num)
-
-        # Balance the heaps by adjusting their sizes if necessary
-        if len(self.small) > len(self.large) + 1:
-            val = -1 * heapq.heappop(self.small)
-            heapq.heappush(self.large, val)
-        if len(self.large) > len(self.small) + 1:
-            val = heapq.heappop(self.large)
-            heapq.heappush(self.small, -1 * val)
+            heappush(self.small, -heappushpop(self.large, num))
+        
 
     def findMedian(self) -> float:
-        # If sizes of both heaps are equal, return the average of the maximum element in the small heap
-        # and the minimum element in the large heap
-        if len(self.small) > len(self.large):
-            return -1 * self.small[0]
-        elif len(self.large) > len(self.small):
-            return self.large[0]
-        return (-1 * self.small[0] + self.large[0]) / 2.0
+        if len(self.small) == len(self.large):
+            return float(self.large[0] - self.small[0]) / 2.0
+        else:
+            return float(self.large[0])
         
 
 
