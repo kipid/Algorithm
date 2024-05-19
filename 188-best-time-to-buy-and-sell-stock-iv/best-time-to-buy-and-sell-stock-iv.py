df31a330f5364587eb18c74828fac64168bdf4c5
Kang-Soo Lee <kipacti@gmail.com>
2024-05-19 11:04:17 +0900

    Time: 87 ms (84.16%) | Memory: 16.6 MB (90.64%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -1,16 +1,11 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        n = len(prices)
-        # maintaing a dp matrix 
-        dp = [[[0 for _ in range(k+1)] for _ in range(2)] for _ in range(n+1)]
-        # Traversing the prices inversely and buy can be 0 or 1 if 0 means we can buy and 1 means we cannot
-        for i in range(n-1,-1,-1):
-            for buy in range(2):
-                for cap in range(1,k+1):
-                    if buy == 0:
-                        # If we are buying then the capacity remains same as we have not sell it yet 
-                        # If we buy we add - to the price as we have invested the money
-                        dp[i][buy][cap] = max(0+dp[i+1][0][cap],-prices[i]+dp[i+1][1][cap])
-                    elif buy == 1: # Selling 
-                        dp[i][buy][cap] = max(0+dp[i+1][1][cap], prices[i]+dp[i+1][0][cap-1])
-        return dp[0][0][k]
+        min_price = [inf] * (k + 1)
+        max_profit = [0] * (k + 1)
+
+        for price in prices:
+            for i in range(1, k + 1):
+                min_price[i] = min(min_price[i], price - max_profit[i - 1])
+                max_profit[i] = max(max_profit[i], price - min_price[i])
+
+        return max_profit[k]
