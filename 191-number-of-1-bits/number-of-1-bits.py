8df021f4872b5dcf1ee7e0d173995cc24e653e90
Kang-Soo Lee <kipacti@gmail.com>
2024-08-11 21:04:56 +0900

    Time: 39 ms (33.81%) | Memory: 16.5 MB (64.90%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,9 +1,7 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        sum = 0
-        while (n // 2) != 0:
-            # print(f"{n = }, {n%2 = }")
-            sum += n % 2
+        w = 0
+        while (n > 0):
+            w += n % 2
             n = n // 2
-        sum += n % 2
-        return sum
+        return w
