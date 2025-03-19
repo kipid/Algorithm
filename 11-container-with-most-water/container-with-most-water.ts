e1701df191c1e1ed75ec8b59d83d84137783db33
Kang-Soo Lee <kipacti@gmail.com>
2025-03-19 15:07:34 +0900

    Time: 2 ms (79.96%) | Memory: 65.9 MB (5.93%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.ts
+++ 11-container-with-most-water/container-with-most-water.ts
@@ -2,12 +2,7 @@ function maxArea(height: number[]): number {
     const n = height.length;
     let left = 0;
     let right = n-1;
-    let max = Math.min(height[left], height[right]) * (right-left);
-    if (height[left] < height[right]) {
-        left++;
-    } else {
-        right--;
-    }
+    let max = 0;
     while (left < right) {
         max = Math.max(max, Math.min(height[left], height[right]) * (right-left));
         if (height[left] < height[right]) {
